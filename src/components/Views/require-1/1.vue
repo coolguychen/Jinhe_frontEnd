2e9ad6c5ce9e3445e9437c481918ff784d66897a
xuqiang124 <dzq_123@163.com>
2021-11-26 17:00:09 +0800

    传参

--- src/components/Views/require-1/1.vue
+++ src/components/Views/require-1/1.vue
@@ -3,7 +3,7 @@
     <el-row type="flex" class="row-input" justify="center">
       <el-col :span="6">
         <el-input
-          v-model="input"
+          v-model="id"
           placeholder="请输入要查询的线路名"
           clearable
         ></el-input>
@@ -12,6 +12,19 @@
         <el-button type="primary" @click="searchByName">搜索</el-button>
       </el-col>
     </el-row>
+    <el-descriptions title="基本路线信息" direction="vertical" :column="4" border>
+      <el-descriptions-item label="路线名">name</el-descriptions-item>
+      <el-descriptions-item label="线路类型">干线</el-descriptions-item>
+      <el-descriptions-item label="路线走向">燎原-北路湾公交站</el-descriptions-item>
+      <el-descriptions-item label="方向性">
+        <el-tag size="small">双向</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="运行时间">6：30-22：30</el-descriptions-item>
+      <el-descriptions-item label="单程时间">约49分</el-descriptions-item>
+      <el-descriptions-item label="单向里程">12.0km</el-descriptions-item>
+      <el-descriptions-item label="班次间隔">8</el-descriptions-item>
+
+    </el-descriptions>
   </div>
 </template>
 
@@ -22,12 +35,15 @@ import request from '../../../utils/request'
 export default {
   data () {
     return {
-      input: ''
+      id: ''
     }
   },
   methods: {
     searchByName () {
-      request.get('/basic/get?id=17', {}).then(res => {
+      console.log(this.id)
+      request.get('/basic/getLineInfo', { params:{
+          id: this.id
+        }}).then(res => {
         console.log(res)
       })
     }
