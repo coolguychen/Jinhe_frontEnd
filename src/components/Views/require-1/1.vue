94ea6a8434be885abd932aa6bb8da74f6d801e1d
xuqiang124 <dzq_123@163.com>
2021-11-26 17:51:51 +0800

    需求1

--- src/components/Views/require-1/1.vue
+++ src/components/Views/require-1/1.vue
@@ -12,19 +12,25 @@
         <el-button type="primary" @click="searchByName">搜索</el-button>
       </el-col>
     </el-row>
-    <el-descriptions title="基本路线信息" direction="vertical" :column="4" border>
-      <el-descriptions-item label="路线名">name</el-descriptions-item>
-      <el-descriptions-item label="线路类型">干线</el-descriptions-item>
-      <el-descriptions-item label="路线走向">燎原-北路湾公交站</el-descriptions-item>
-      <el-descriptions-item label="方向性">
-        <el-tag size="small">双向</el-tag>
-      </el-descriptions-item>
-      <el-descriptions-item label="运行时间">6：30-22：30</el-descriptions-item>
-      <el-descriptions-item label="单程时间">约49分</el-descriptions-item>
-      <el-descriptions-item label="单向里程">12.0km</el-descriptions-item>
-      <el-descriptions-item label="班次间隔">8</el-descriptions-item>
 
-    </el-descriptions>
+    <subDialog v-show="dialog_visible">
+      <el-descriptions title="基本路线信息" direction="vertical" :column="4" border>
+        <el-descriptions-item label="路线名">{{name}}</el-descriptions-item>
+        <el-descriptions-item label="线路类型">{{type}}</el-descriptions-item>
+        <el-descriptions-item label="路线走向">{{route}}</el-descriptions-item>
+        <el-descriptions-item label="方向性">
+          <el-tag size="small">{{ directional ? '双向' : '单向/环线' }}</el-tag>
+        </el-descriptions-item>
+        <el-descriptions-item label="运行时间">{{runtime}}</el-descriptions-item>
+        <el-descriptions-item label="单程时间">{{onewayTime}}</el-descriptions-item>
+        <el-descriptions-item label="单向里程(km)">{{kilometer}}</el-descriptions-item>
+        <el-descriptions-item label="班次间隔">{{interval}}</el-descriptions-item>
+
+      </el-descriptions>
+
+    </subDialog>
+
+
   </div>
 </template>
 
@@ -35,7 +41,16 @@ import request from '../../../utils/request'
 export default {
   data () {
     return {
-      id: ''
+      id: '',
+      name: '',
+      route: '',
+      onewayTime: '',
+      directional: '',
+      kilometer: '',
+      runtime: '',
+      interval: '',
+      type: '',
+      dialog_visible: false
     }
   },
   methods: {
@@ -45,6 +60,15 @@ export default {
           id: this.id
         }}).then(res => {
         console.log(res)
+        this.type = res.type
+        this.name = res.name
+        this.route = res.route
+        this.directional = res.directional
+        this.runtime = res.runtime
+        this.onewayTime = res.onewayTime
+        this.kilometer = res.kilometer
+        this.interval = res.interval
+        this.dialog_visible = true
       })
     }
   }
