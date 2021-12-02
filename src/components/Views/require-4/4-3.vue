89867cdbce55383fc3dd7051845fed58b7452d70
coolguychen <1098420273@qq.com>
2021-12-02 21:01:50 +0800

    需求12：4-3.vue 需求16：4-7.vue 环形图和柱状图。

--- src/components/Views/require-4/4-3.vue
+++ src/components/Views/require-4/4-3.vue
@@ -1,2 +1,99 @@
 <!--需求12 分组统计常规公交(包括干线、支线、城乡线、驳接线、社区线)、-->
 <!--快速公交(K字开头)、高峰公交(G字开头)、夜班公交(N字开头)的数量。-->
+<!--用e-chart显示结果-->
+
+<template>
+  <div>
+    <div id="MyChart"
+         style="width: 800px;height:600px;margin-top: 50px;margin-left: 100px"
+    ></div>
+  </div>
+</template>
+
+
+<script>
+import request from "../../../utils/request"
+import * as echarts from 'echarts';
+
+export default {
+  data () {
+    return {
+    }
+  },
+  //通常是为methods函数提前定义（类似提前声明变量 进入页面内容全部渲染完成后自动引函数）
+  mounted() {
+    //画E-Chart图
+    this.typesOfLine()
+  },
+
+  methods:{
+    typesOfLine(){
+      request.get('/analysis/linesByType', {
+        params:{
+        }
+      }).then(res => {
+        console.log(res)
+        let table_data = res.data
+        //声明json数组对象 其中格式为 [{value: '' ,name: ''}]
+        //替换json中的键名 原来是key value，现在要改成name value,值不变
+        let chart_data = table_data.map(function (item){
+          return{
+            name: item.key,
+            value: item.value
+          }
+        });
+        this.draw(chart_data)
+      })
+    },
+    draw(chart_data){
+      // 基于准备好的dom，初始化echarts实例
+      const myChart = echarts.init(document.getElementById("MyChart"));
+      let option = {
+        title: {
+          text: '分组统计不同类型的公交的数量',
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left'
+        },
+        series: [
+          {
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            itemStyle: {
+              borderRadius: 10,
+              borderColor: '#fff',
+              borderWidth: 2
+            },
+            label: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: '40',
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data: chart_data
+          }
+        ]
+      };
+      myChart.setOption(option);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Views/require-4/4-7.vue
+++ src/components/Views/require-4/4-7.vue
@@ -1,1 +1,108 @@
 <!--需求16 根据站点数量对线路进行排序-->
+
+<!--/**-->
+<!--* 16. 根据站点数量对线路进行排序-->
+<!--* @return 线路名&相应站点数-->
+<!--*/-->
+<!--Map<String, Integer> sortLinesByStations();-->
+<!--尝试用柱状图-->
+
+<template>
+  <div>
+    <div id="MyChart"
+         style="width: 800px;height:800px;margin-left: 100px"
+    ></div>
+  </div>
+
+</template>
+
+<script>
+import request from "../../../utils/request"
+import * as echarts from 'echarts';
+
+export default {
+  data () {
+    return {
+    }
+  },
+  //通常是为methods函数提前定义（类似提前声明变量 进入页面内容全部渲染完成后自动引函数）
+  mounted() {
+    this.sortLinesByStations()
+  },
+  methods:{
+    sortLinesByStations(){
+      request.get('/analysis/linesByStationNum', {
+        params:{
+        }
+      }).then(res => {
+        console.log(res.data[0])
+        let chart_data = res.data
+        //声明json数组对象
+        let category = []
+        let value = []
+        //this.tableData = res.data
+        for(var i=0; i<chart_data.length; i++){
+          category.push(chart_data[i].key) //json数组添加值用push
+          value.push(chart_data[i].value)
+        }
+        //降序排序
+        category = category.reverse()
+        value = value.reverse()
+        console.log(category,value)
+        this.draw(category,value)
+      })
+    },
+    draw(category, value){
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(document.getElementById("MyChart"));
+      let option = {
+        title: {
+          text: '站点数量最多的15条线路'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        legend: {},
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'value',
+          boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+          type: 'category',
+          data: category
+        },
+        series: [
+          {
+            name: '站点数量',
+            type: 'bar',
+            data: value,
+            itemStyle: {        //上方显示数值
+              normal: {
+                color: "#9bd3e8",
+                label: {
+                  show: true, //开启显示
+                  position: 'right', //在上方显示
+                  textStyle: { //数值样式
+                    color: 'grey',
+                    fontSize: 12
+                  }
+                }
+              }
+            }
+          }
+        ]
+      };
+      myChart.setOption(option)
+    }
+  }
+}
+</script>
