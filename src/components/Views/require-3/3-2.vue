d42682f6d08d1b918774578c662c257c67bd135c
coolguychen <1098420273@qq.com>
2021-12-02 22:49:05 +0800

    需求8（3-2.vue）完成！

--- src/components/Views/require-3/3-2.vue
+++ src/components/Views/require-3/3-2.vue
@@ -8,79 +8,82 @@
 <!--*/-->
 <template>
   <div style="margin-left: 30px">
-    <div>
-      <el-descriptions title="请选择时间：">
-
-      </el-descriptions>
-
-      <!--    某个时刻--><!--当前时间-->
-      <el-time-picker
-        v-model="time"
-        format='HH:mm'
-        value-format="HH:mm"
-        clearable
-        :picker-options="{
-        selectableRange:'00:00:00 - 23:59:00'
-    }">
-      </el-time-picker>
-    </div>
-
-    <!--    某个站台-->
-    <div style="margin-top: 15px">
+    <!--input-->
+    <!--某个站台-->
+    <div >
       <el-descriptions title="请输入站台ID：">
-
       </el-descriptions>
       <el-input style="width: 250px" clearable v-model="id" placeholder="请输入站台ID，如16147">
-
       </el-input>
     </div>
 
-    <!--    某个时段-->
     <div style="margin-top: 15px">
-      <el-descriptions title="请输入时段：">
-      </el-descriptions>
-      <el-time-picker
-        v-model="interval"
-        format='mm'
-        value-format="mm"
-        clearable
-        :picker-options="{
-        selectableRange:'00:00:00 - 00:59:00'
-    }">
-      </el-time-picker>
+      <el-row type="flex" class="row-input" >
+        <el-col span="6">
+          <!--某个时刻-->
+          <el-descriptions title="请选择时间：">
+          </el-descriptions>
+          <el-time-picker
+            v-model="time"
+            format='HH:mm'
+            value-format="HH:mm"
+            clearable
+            :picker-options="{
+              selectableRange:'00:00:00 - 23:59:00'
+            }">
+          </el-time-picker>
+        </el-col>
+
+        <el-col span="6" >
+          <!--某个时段-->
+          <el-descriptions title="请输入时段：">
+          </el-descriptions>
+          <el-time-picker
+            v-model="interval"
+            format='mm'
+            value-format="mm"
+            clearable
+            :picker-options="{
+              selectableRange:'00:00:00 - 00:59:00'
+            }">
+          </el-time-picker>
+        </el-col>
+      </el-row>
     </div>
-    <el-button style="margin-top: 15px" type="primary" @click="queryNextLinesToCome" >搜索</el-button>
+
+    <el-button style="margin-top: 20px" type="primary" @click="queryNextLinesToCome" >搜索</el-button>
+
     <!--    输出即将停靠的线路-->
-
-    <el-descriptions style="margin-top: 20px" title="最近班次：">
-    </el-descriptions>
-
-    <div style="margin-left: 20px">
-      <el-table :data="tableData"
-                stripe border
-                style="width:601px; margin-left:50px; text-align: center;
+    <subDialog v-show="dialog_visible">
+      <el-descriptions style="margin-top: 20px" title="最近班次：">
+      </el-descriptions>
+      <div class="next" style="max-height: 50vh; margin-left: 20px">
+        <el-table :data="tableData"
+                  stripe border
+                  style="width:801px; margin-left:30px; text-align: center;
                 align-content: center"
-      >
-        <el-table-column
-          prop="key"
-          label="班次"
-          width="300"
-          align="center"
         >
-        </el-table-column>
-        <el-table-column
-          prop="value"
-          label="到站时间"
-          align="center"
-          width="300">
-          <template slot-scope="scope">
-            {{ scope.row.value == 0 ? '即将到站' : scope.row.value + '分钟后到站' }}
-          </template>
-        </el-table-column>
+          <el-table-column
+            prop="key"
+            label="线路名"
+            width="400"
+            align="center"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="value"
+            label="到站时间"
+            align="center"
+            width="400">
+            <template slot-scope="scope">
+              {{ scope.row.value == 0 ? '即将到站' : scope.row.value + '分钟后到站' }}
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </subDialog>
 
-      </el-table>
-
-      <subDialog v-show="dialog_visible">
+      <subDialog v-show="error_dialog">
         <el-alert
           title="错误提示："
           type="error"
@@ -88,8 +91,6 @@
           show-icon>
         </el-alert>
       </subDialog>
-
-    </div>
   </div>
 </template>
 
@@ -101,7 +102,8 @@ export default {
     return {
       id:'',
       time:'',
-      dialog_visible:false,
+      dialog_visible: false,
+      error_dialog: false,
       interval:'',
       msg:'',
       tableData:[]
@@ -120,14 +122,24 @@ export default {
         console.log(res)
         if(res.result == false){
           this.msg = res.msg
-          this.dialog_visible = true
+          //搜索失败，隐藏表格并提示错误
+          this.error_dialog = true
+          this.dialog_visible = false
         }
         else{
           this.tableData = res.data
-          this.dialog_visible = false //搜索成功 取消错误提示
+          this.dialog_visible = true
+          this.error_dialog = false //搜索成功 取消错误提示
         }
       })
     }
   }
 }
 </script>
+
+<style>
+.next{
+  overflow-x: hidden;
+  overflow-y: visible;
+}
+</style>
